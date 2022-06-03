ebaef7af2e6dd06fbb32d0d719b079d824cb1812
ariviegea2 <ariadna.villar.egea@volvocars.com>
2022-06-03 11:47:44 +0200

    Removed footer and other styling

--- discover-coffee-stores/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return(
-        <>
-        <div style={{backgroundColor: 'blue', padding: '30px'}}> I am a footer</div>
-        </>
-    )
-}
-
-export default Footer;
--- discover-coffee-stores/pages/_app.js
+++ discover-coffee-stores/pages/_app.js
@@ -1,10 +1,9 @@
 import '../styles/globals.css'
-import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
   
-  <><Component {...pageProps} /><Footer /></>
+  <><Component {...pageProps} /></>
   
   
   )
