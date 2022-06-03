0a0b06bcc0f45451e6a9c0f63336475d06200a67
ariviegea2 <ariadna.villar.egea@volvocars.com>
2022-06-03 10:45:16 +0200

    Created ugly footer that renders everywhere in the application

---
+++ discover-coffee-stores/components/Footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <>
+        <div style={{backgroundColor: 'blue', padding: '30px'}}> I am a footer</div>
+        </>
+    )
+}
+
+export default Footer;
